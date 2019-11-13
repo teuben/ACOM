bcc28623e106a96d46acf0f45a99f902707835e3
Peter Teuben <teuben@gmail.com>
2019-11-13 15:59:15 -0500

    fix

--- eagle/eagle.sh
+++ eagle/eagle.sh
@@ -7,8 +7,10 @@
       "http://dataweb.cosma.dur.ac.uk:8080/eagle-snapshots//download?run=RefL0012N0188&snapnum=0"
     tar xvf RefL0012N0188_snap_028.tar 
     tar xvf RefL0012N0188_snap_000.tar
-#  
-    h5dump RefL0012N0188/snapshot_028_z000p000/snap_028_z000p000.0.hdf5 | more
+#
+#   or do something like:     ln -s ~/EAGLE/RefL0012N0188
+#       
+    h5dump RefL0012N0188/snapshot_028_z000p000/snap_028_z000p000.0.hdf5 | head 
 #  
     eaglesnap RefL0012N0188/snapshot_028_z000p000/snap_028_z000p000.0.hdf5 snap1  ptype=0 region=2
     tsf snap1 
@@ -22,7 +24,7 @@
     snapplot3 - xrange=-10:10 yrange=-10:10 zrange=-10:10 yapp=eagle_2.png/png 
    
 #   
-    eaglesnap snap_028_z000p000.0.hdf5 - 0 group=4 center=1.9,6.6,1.2 boxsize=8.47125 |\
+    eaglesnap RefL0012N0188/snapshot_028_z000p000/snap_028_z000p000.0.hdf5 - 0 group=4 center=1.9,6.6,1.2 boxsize=8.47125 |\
     hackforce - - |\
     snapcenter - - '-phi*phi*phi' |\
     snaprect - snap4cr '-phi*phi*phi'
@@ -33,26 +35,29 @@
     s=0.1
     snapgrid snap4cr ccd0s xrange=-$s:$s yrange=-$s:$s nx=128 ny=128 mom=0  svar=0.001
     snapgrid snap4cr ccd1s xrange=-$s:$s yrange=-$s:$s nx=128 ny=128 mom=-1 svar=0.001
-    ccdplot ccd0s power=0.5
-    ccdplot ccd1s eagle_5.png/png 
+    ccdplot ccd0s power=0.5 yapp=eagle_5.png/png 
+    ccdplot ccd1s 
 
 #    
     b=8.47125      # box size
     dm=4365.77     # cheat to get the correct DM amount
+    snap0=RefL0012N0188/snapshot_028_z000p000/snap_028_z000p000.0.hdf5     # at z=0
+    snap20=RefL0012N0188/snapshot_000_z020p000/snap_000_z020p000.0.hdf5    # at z=20
 
-    eaglesnap snapshot_028_z000p000/snap_028_z000p000.0.hdf5 - 0 |\
+#    
+    eaglesnap $snap0  - 0 |\
     snapgridsmooth - -  xrange=0:$b yrange=0:$b zrange=0:$b nx=128 ny=128 nz=128 periodic=t |\
     ccdsmooth - gas0s 0.1 dir=xyz
   
-    eaglesnap snapshot_028_z000p000/snap_028_z000p000.0.hdf5 - 1 dm=$dm |\
+    eaglesnap $snap0 - 1 dm=$dm |\
     snapgridsmooth - -  xrange=0:$b yrange=0:$b zrange=0:$b nx=128 ny=128 nz=128 periodic=t |\
     ccdsmooth - dm0s 0.1 dir=xyz
 
-    eaglesnap snapshot_000_z020p000/snap_000_z020p000.0.hdf5 - 0 |\
+    eaglesnap $snap20 - 0 |\
     snapgridsmooth - -  xrange=0:$b yrange=0:$b zrange=0:$b nx=128 ny=128 nz=128 periodic=t |\
     ccdsmooth - gas20s 0.1 dir=xyz
 
-    eaglesnap snapshot_000_z020p000/snap_000_z020p000.0.hdf5 - 1 dm=$dm |\
+    eaglesnap $snap20 - 1 dm=$dm |\
     snapgridsmooth - -  xrange=0:$b yrange=0:$b zrange=0:$b nx=128 ny=128 nz=128 periodic=t |\
     ccdsmooth - dm20s 0.1 dir=xyz
 
